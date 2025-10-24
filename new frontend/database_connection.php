cee61174bf9586fba02dbdff30d1d7fe2b9cfc3b
walter-bubble <munenewalter486@gmail.com>
2025-10-24 18:22:58 +0300

    u

---
+++ new frontend/database_connection.php
@@ -0,0 +1,32 @@
+<?php
+// db.php
+/*
+$host = "localhost";
+$dbname = "hotel_management";
+$user = "root";
+$password = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "✅ Database connected successfully";
+} catch (PDOException $e) {
+    die("❌ Connection failed: " . $e->getMessage());
+}
+*/
+
+$host = 'localhost';
+$port = 5432; // default PostgreSQL port
+$dbname = 'Hotel_management';
+$user = 'postgres';
+$password = '8448';
+
+
+try {
+    // Change mysql: to pgsql:
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("❌ Connection failed: " . $e->getMessage());
+}
+?>
